842a6f88580bd81ac6ccc8871da4339c86a7eada
Rathkalyfogna <forgna@wonderpass.asia>
2021-07-20 18:51:22 +0700

    Add post

---
+++ pages/posts/[slug].js
@@ -0,0 +1,7 @@
+export default function PostPage() {
+    return <div>
+        <main>
+            <h1>Post [slug]</h1>
+        </main>
+    </div>
+}
---
+++ pages/posts/index.js
@@ -0,0 +1,11 @@
+export default function PostsPage() {
+    return <div>
+        <main>
+            <ul>
+                <li>Post 1</li>
+                <li>Post 2</li>
+                <li>Post 3</li>
+            </ul>
+        </main>
+    </div>
+}
